2937c0d604650d3f86be852654c4481f1921fe0d
canel <back.up.canel@gmail.com>
2022-03-25 13:22:43 +0100

    smol tweaks
    slight changes idk

--- data.py
+++ data.py
@@ -8,10 +8,10 @@ def store_data(temp, pres, hum):
     # Store the values in a json file
     data = {
         
-        "date": str(now.strftime("%d.%m.%Y//%H:%M")),
-        "temperature": temp,
-        "pressure": pres,
-        "humidity": hum
+        "Date/Time": str(now.strftime("%d.%m.%Y//%H:%M")),
+        "Temperature": int(temp),
+        "Pressure": int(pres),
+        "Humidity": int(hum)
     }
     with open('/home/pi/Desktop/schule/software_wetter_station/main_program/data.json', 'w') as outfile:
         json.dump(data, outfile)
